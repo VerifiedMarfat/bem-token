82df8d525707ea87db97527fb92b7723a93537ed
vytautas brundza <vytautas.brundza@cohaesus.co.uk>
2015-11-14 11:32:06 +0000

    create product page layout

--- product.php
+++ product.php
@@ -17,17 +17,37 @@
     <?php //### CONTENT ### ?>
     <main>
         <div class="wrapper">
-
-            <img src="http://placekitten.com/g/550/550" alt="">
-            <h1>Product Name</h1>
-            <p>
-                Duis vehicula laoreet lacus sit amet luctus. Donec tempor pulvinar nunc nec rhoncus. Nullam pulvinar placerat nisi ac viverra. Nullam egestas, lacus quis fermentum rhoncus, magna enim consectetur nunc, in fringilla leo felis sit amet sem. Nunc nisi nunc, vehicula mattis justo a, tempor convallis lectus. Morbi ac ante tortor. Fusce vitae nunc ut nunc vehicula luctus id nec ex.
-            </p>
-
-            <form action="">
-                <input type="submit" name="checkout">
-            </form>
-
+            <div class="grid">
+                <div class="grid__item one-third palm-one-whole">
+                    <form action="">
+                        <input type="submit" value="Buy" name="checkout" class="btn one-whole push--bottom">
+                    </form>
+                    <img src="http://placekitten.com/g/550/550" alt="">
+                </div><!--
+             --><div class="grid__item two-thirds palm-one-whole">
+                    <h1>Product Name</h1>
+                    <p>
+                        Duis vehicula laoreet lacus sit amet luctus. Donec tempor pulvinar nunc nec rhoncus. Nullam pulvinar placerat nisi ac viverra. Nullam egestas, lacus quis fermentum rhoncus, magna enim consectetur nunc, in fringilla leo felis sit amet sem. Nunc nisi nunc, vehicula mattis justo a, tempor convallis lectus. Morbi ac ante tortor. Fusce vitae nunc ut nunc vehicula luctus id nec ex.
+                    </p>
+                    <p><strong># <span class="js-percent1"></span>%</strong></p>
+                    <p><strong># <span class="js-percent2"></span>%</strong></p>
+                    <p><strong># <span class="js-ml"></span>ml</strong></p>
+                </div>
+            </div>
+            <div class="product-bottom product-price-cnt">
+                <div class="push--bottom push-half--right">
+                    Price <span class="product-price push-half--left">$$$</span>
+                </div>
+                <form action="">
+                    <input type="submit" value="Buy" name="checkout" class="btn one-third lap-two-thirds palm-one-whole">
+                </form>
+                <div class="social">
+                    <ul class="nav nav--block">
+                        <li><a class="social--icon" href=""><span class="icon-facebook--after"></span></a></li>
+                        <li><a class="social--icon" href=""><span class="icon-twitter--after"></span></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </main>
 
