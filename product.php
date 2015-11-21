a5dc2667e0c1d22356634f647e259791455c1063
VT <marfat.vt@gmail.com>
2015-11-21 16:16:06 +0000

    Minor changes to the product page.

--- product.php
+++ product.php
@@ -4,54 +4,49 @@
 <!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 
-<?php //### HEAD ###
+<?php 
     include 'partial-views/head.php';
 ?>
 
 <body id="trumps">
 
-    <?php //### HEADER ###
+    <?php 
         include 'partial-views/header.php';
     ?>
 
-    <?php //### CONTENT ### ?>
-    <main>
-        <div class="wrapper soft--top">
-            <div class="grid">
-                <div class="grid__item one-third palm-one-whole">
-                    <form action="">
-                        <input type="submit" value="Buy" name="checkout" class="btn one-whole push--bottom">
-                    </form>
-                    <img src="http://placekitten.com/g/550/550" alt="">
-                </div><!--
-             --><div class="grid__item two-thirds palm-one-whole">
-                    <h1>Product Name</h1>
-                    <p>
-                        Duis vehicula laoreet lacus sit amet luctus. Donec tempor pulvinar nunc nec rhoncus. Nullam pulvinar placerat nisi ac viverra. Nullam egestas, lacus quis fermentum rhoncus, magna enim consectetur nunc, in fringilla leo felis sit amet sem. Nunc nisi nunc, vehicula mattis justo a, tempor convallis lectus. Morbi ac ante tortor. Fusce vitae nunc ut nunc vehicula luctus id nec ex.
-                    </p>
-                    <p><strong># <span class="js-percent1"></span>%</strong></p>
-                    <p><strong># <span class="js-percent2"></span>%</strong></p>
-                    <p><strong># <span class="js-ml"></span>ml</strong></p>
-                </div>
+    <main class="wrapper soft--top">
+        <div class="grid">
+            <div class="grid__item one-third palm-one-whole">
+                <img src="http://placekitten.com/g/550/550" alt="">
+            </div><!--
+         --><div class="grid__item two-thirds palm-one-whole">
+                <h1>Product Name</h1>
+                <p>
+                    Duis vehicula laoreet lacus sit amet luctus. Donec tempor pulvinar nunc nec rhoncus. Nullam pulvinar placerat nisi ac viverra. Nullam egestas, lacus quis fermentum rhoncus, magna enim consectetur nunc, in fringilla leo felis sit amet sem. Nunc nisi nunc, vehicula mattis justo a, tempor convallis lectus. Morbi ac ante tortor. Fusce vitae nunc ut nunc vehicula luctus id nec ex.
+                </p>
+                <p><strong># <span class="js-percent1"></span>%</strong></p>
+                <p><strong># <span class="js-percent2"></span>%</strong></p>
+                <p><strong># <span class="js-ml"></span>ml</strong></p>
             </div>
-            <div class="product-bottom product-price-cnt">
-                <div class="push--bottom push-half--right">
-                    Price <span class="product-price push-half--left">$$$</span>
-                </div>
-                <form action="">
-                    <input type="submit" value="Buy" name="checkout" class="btn one-third lap-two-thirds palm-one-whole">
-                </form>
-                <div class="social">
-                    <ul class="nav nav--block">
-                        <li><a class="social--icon" href=""><span class="icon-facebook--after"></span></a></li>
-                        <li><a class="social--icon" href=""><span class="icon-twitter--after"></span></a></li>
-                    </ul>
-                </div>
+        </div>
+        <div class="text--right">
+            <div class="push--bottom">
+                <span class="push--sides">Price</span>
+                <span class="+lined">$$$</span>
+            </div>
+            <form action="" class="text--right">
+                <input type="submit" value="Buy" name="checkout" class="btn one-third lap-two-thirds palm-one-whole">
+            </form>
+            <div class="social">
+                <ul class="nav nav--block">
+                    <li><a class="social--icon" href=""><span class="icon-facebook--after"></span></a></li>
+                    <li><a class="social--icon" href=""><span class="icon-twitter--after"></span></a></li>
+                </ul>
             </div>
         </div>
     </main>
 
-    <?php //### FOOTER ###
+    <?php 
         include 'partial-views/footer.php';
     ?>
 
